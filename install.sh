01e73db8e4597f4652033e829a6e4f04d86be774
Konstantin Nikolaev <knikolaev@ptsecurity.com>
2017-03-07 14:52:36 +0700

    Fix 0.1

--- install.sh
+++ install.sh
@@ -2,9 +2,10 @@
 
 vim --version || (apt-get update && apt-get install -y vim)
 cp vimrc /etc/vim/vimrc
+echo "Configure VIM for root user"
+vim +PluginInstall +qall
+
 ls /home | while read username; do
   echo "Configure VIM for $username user"
-  cp -r vim /home/$username/.vim
-  chown -R $username /home/$username/.vim
-#  su $username -c 'vim +PluginInstall +qall'
+  su - $username -c 'vim +PluginInstall +qall'
 done
